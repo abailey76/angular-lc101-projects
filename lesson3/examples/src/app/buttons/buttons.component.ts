8699137329e896621cadde3e6cc0fb7438e7b971
Adam Bailey <abailey76@duck.com>
2021-11-15 13:16:11 -0600

    angular 30.6 exercises angular part 3

--- lesson3/examples/src/app/buttons/buttons.component.ts
+++ lesson3/examples/src/app/buttons/buttons.component.ts
@@ -7,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ButtonsComponent implements OnInit {
    buttonHeading: string = "Buttons"
-   inactive: boolean = false;
+   // inactive: boolean = false;
+   goldInactive: boolean = false;
+   silverInactive: boolean = false;
+   copperInactive: boolean = false;
 
    constructor() { }
 
    ngOnInit() { }
+
+   reactivate() {
+      this.goldInactive = false;
+      this.silverInactive = false;
+      this.copperInactive = false;
+   }
 
 }
